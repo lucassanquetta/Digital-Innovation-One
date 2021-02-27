6916a727d264d10b02ebfffbcc850fb0f7c94ea4
Lucas dos Reis Nogueira Sanquetta <45104306+lucassanquetta@users.noreply.github.com>
2021-02-27 02:07:21 -0300

    Update Program.cs

--- Bootcamps/MRV .NET Developer/Aprenda o que são Estrutura de Dados e Algoritmos/Aulas_Estrutura_de_Dados/Program.cs
+++ Bootcamps/MRV .NET Developer/Aprenda o que são Estrutura de Dados e Algoritmos/Aulas_Estrutura_de_Dados/Program.cs
@@ -5,7 +5,7 @@ namespace Aulas_Estrutura_de_Dados
     class Program
     {
         static void Main(string[] args)
-        {
+        {//teste 02
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
