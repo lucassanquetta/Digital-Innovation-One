d4059501c84db88632afdcc75f0647562ca1821a
lucassanquetta <lucassanquetta@fatec.sp.gov.br>
2021-02-27 02:06:10 -0300

    Update Program.cs

--- Bootcamps/MRV .NET Developer/Aprenda o que são Estrutura de Dados e Algoritmos/Aulas_Estrutura_de_Dados/Program.cs
+++ Bootcamps/MRV .NET Developer/Aprenda o que são Estrutura de Dados e Algoritmos/Aulas_Estrutura_de_Dados/Program.cs
@@ -5,7 +5,7 @@ namespace Aulas_Estrutura_de_Dados
     class Program
     {
         static void Main(string[] args)
-        {//teste
+        {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
